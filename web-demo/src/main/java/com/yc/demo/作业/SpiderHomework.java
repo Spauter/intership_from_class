69c1cd4fce09d95c65b4887ac12b76edb1e1e3b4
Spauter <3230695439@qq.com>
2024-03-15 20:10:50 +0800

    删除eclipse中.class文件 自定义bean容器

--- web-demo/src/main/java/com/yc/demo/作业/SpiderHomework.java
+++ web-demo/src/main/java/com/yc/demo/作业/SpiderHomework.java
@@ -1,10 +1,9 @@
 package com.yc.demo.作业;
 
 
-//import blo.spau.excel.ExcelUtil;
 import blo.spau.excel.ExcelUtil;
-import blo.spau.excel.output.Output;
-import blo.spau.excel.read.Read;
+import blo.spau.excel.output.OutputExcel;
+import blo.spau.excel.read.ReadExcel;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.select.Elements;
@@ -19,16 +18,16 @@ import java.util.*;
  * @see <a href="https://www.0734zpw.com">...</a>
  */
 public class SpiderHomework {
-    static Set<Map<String,Object>> set=new HashSet<>();
+    static Set<Map<String, Object>> set = new HashSet<>();
 
     public static void main(String[] args) throws IOException {
-        final URL url=new URL("https://www.0734zpw.com");
-        for(int i=1;i<=2;i++){
-            Document document= Jsoup.parse(new URL(url+"/offer-"+i),5000);
+        final URL url = new URL("https://www.0734zpw.com");
+        for (int i = 1; i <= 2; i++) {
+            Document document = Jsoup.parse(new URL(url + "/offer-" + i), 5000);
             final Elements a = document.select("a");
             a.forEach(a1 -> {
-                String href=a1.attr("href");
-                if (href.matches("/c\\d+\\.html")){
+                String href = a1.attr("href");
+                if (href.matches("/c\\d+\\.html")) {
                     try {
                         getInfo(href);
                     } catch (IOException e) {
@@ -37,34 +36,32 @@ public class SpiderHomework {
                 }
             });
         }
-        for (Map<String,Object> m:set){
+        for (Map<String, Object> m : set) {
             System.out.println(m);
         }
-        List<Map<String,Object>>list=new ArrayList<>();
-        list.addAll(set);
+        List<Map<String, Object>> list = new ArrayList<>(set);
         System.out.println(list.size());
-        Output output=new ExcelUtil();
-        File file=new File(System.getenv("TEMP")+"\\1.xlsx");
-        output.outPut(list,file);
-        Read read=new ExcelUtil();
-        read.readToList(System.getenv("TEMP")+"\\1.xlsx");
-
+        OutputExcel output = new ExcelUtil();
+        File file = new File(System.getenv("TEMP") + "\\1.xlsx");
+        output.outPut(list, file);
+        ReadExcel excel = new ExcelUtil();
+        excel.getTitle();
     }
 
-    public static void getInfo(String id)throws IOException {
-         URL url = new URL("https://www.0734zpw.com" + id);
-         Document document = Jsoup.parse(url, 5000);
-         Elements p = document.select(".Xqcominfo");
+    public static void getInfo(String id) throws IOException {
+        URL url = new URL("https://www.0734zpw.com" + id);
+        Document document = Jsoup.parse(url, 5000);
+        Elements p = document.select(".Xqcominfo");
         String info = p.get(0).text();
         String cname = info.split("关注")[0];
         String cinfo = info.split("关注")[1];
         String[] cinfos = cinfo.split("\\s");
-        Map<String, Object> map=new HashMap<>();
+        Map<String, Object> map = new HashMap<>();
         map.put("公司名称", cname);
         for (String c : cinfos) {
             String[] items = c.split("：");
-            if(items.length==2 && items[0].length()!=1) {
-                    map.put(items[0], items[1]);
+            if (items.length == 2 && items[0].length() != 1) {
+                map.put(items[0], items[1]);
 
             }
         }
