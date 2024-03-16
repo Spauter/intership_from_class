9f1a178bf22d8facc645236c87a72abfee05010e
Spauter <3230695439@qq.com>
2024-03-16 18:45:40 +0800

    删除多余jar包

---
+++ hw-spring-ioc/src/main/java/com/yc/spring/aop/UserDaoProxy.java
@@ -0,0 +1,11 @@
+package com.yc.spring.aop;
+
+import com.yc.spring.bbs.dao.UserDaoImpl;
+import com.yc.spring.bbs.dao.UserDao;
+
+public class UserDaoProxy {
+    UserDao userDao=new UserDaoImpl();
+
+
+
+}
