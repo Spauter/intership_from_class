5c46b24f94bd6f19314626bc10c4d998e913b00c
Spauter <3230695439@qq.com>
2024-04-02 18:35:35 +0800

    补充提交

--- sp_redis_session/src/main/java/com/blo/handler/SessionController.java
+++ sp_redis_session/src/main/java/com/blo/handler/SessionController.java
@@ -3,11 +3,11 @@ package com.blo.handler;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 import java.util.HashMap;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/session")
 public class SessionController {
 
     /**
@@ -23,6 +23,18 @@ public class SessionController {
         map.put("SessionID:",request.getSession().getId());
         return map;
     }
+    /**
+     * 设置session的另一种
+     * @return
+     */
+    @RequestMapping("/set1")
+    public Map<String,Object> setSession(HttpServletRequest request,HttpSession session){
+        String attributes=request.getParameter("attributes");
+        Map<String,Object> map = new HashMap<String,Object>();
+        session.setAttribute("attributes",attributes);
+        map.put("SessionID:",session.getId());
+        return map;
+    }
 
     /**
      * 获取session
@@ -33,5 +45,15 @@ public class SessionController {
     public String getSession(HttpServletRequest request){
         String attributes = (String) request.getSession().getAttribute("attributes");
         return attributes;
+    }
+
+    /**
+     * 另一种
+     * @param session
+     * @return
+     */
+    @RequestMapping("/get1")
+    public String getSession(HttpSession session) {
+        return (String) session.getAttribute("attributes");
     }
 }
