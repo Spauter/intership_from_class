0ff11c90a8e3dbaa3e7a20396cd2563e8e1f02b8
Spauter <3230695439@qq.com>
2024-04-11 17:11:44 +0800

    轮询测试

--- thread_test/src/main/java/com/bloducspauter/futuretest/CompletableFutureDemo.java
+++ thread_test/src/main/java/com/bloducspauter/futuretest/CompletableFutureDemo.java
@@ -1,12 +1,11 @@
 package com.bloducspauter.futuretest;
 
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.FutureTask;
-import java.util.concurrent.TimeUnit;
+import java.util.Scanner;
+import java.util.concurrent.*;
 
 /**
  * Intel core i5-10210U 1.60GHz
+ *
  * @author Bloduc Spauter
  */
 public class CompletableFutureDemo {
@@ -62,5 +61,38 @@ class CompletableFutureDemo2 {
         long endTime = System.currentTimeMillis();
         //about 3ms
         System.out.println("----cost time: " + (endTime - startTime) + "ms");
+    }
+}
+
+class CompletableFutureDemo3 {
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        FutureTask<String> futureTask = new FutureTask<>(() -> {
+            System.out.println(Thread.currentThread().getName() + "------Come In");
+            //sleep  5 seconds
+            TimeUnit.MICROSECONDS.sleep(5000000);
+            return "Task over";
+        });
+        Thread thread = new Thread(futureTask, "t1");
+        thread.start();
+        long startTime = System.currentTimeMillis();
+        /*
+            Once using the method "get", it may be blocked because it's calculate is not finished
+            If you use method "isDome", It may cause CPU idling
+            Uncomment this below code, The following loop statement needs to wait the thread done
+         */
+//        System.out.println(futureTask.get());
+        System.out.println(Thread.currentThread().getName() + "------Busy for other task");
+        long endTime = System.currentTimeMillis();
+        System.out.println("----cost time: " + (endTime - startTime) + "ms");
+        while (true) {
+            if (futureTask.isDone()) {
+                System.out.println(futureTask.get() + "------");
+                break;
+            } else {
+                //To pause 1s
+                TimeUnit.MICROSECONDS.sleep(1000000);
+                System.out.println("The task t1 is running, don't urge!");
+            }
+        }
     }
 }
