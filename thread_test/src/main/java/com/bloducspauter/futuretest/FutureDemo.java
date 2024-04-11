0b99774e07cfe42de43abf1677e2f2c44c5bc38c
Spauter <3230695439@qq.com>
2024-04-11 20:30:18 +0800

    CompletableFuture Test

--- thread_test/src/main/java/com/bloducspauter/futuretest/FutureDemo.java
+++ thread_test/src/main/java/com/bloducspauter/futuretest/FutureDemo.java
@@ -1,14 +1,11 @@
 package com.bloducspauter.futuretest;
-
-import java.util.Scanner;
 import java.util.concurrent.*;
 
 /**
- * Intel core i5-10210U 1.60GHz
- *
+ * CPU:Intel core i5-10210U 1.60GHz
  * @author Bloduc Spauter
  */
-public class CompletableFutureDemo {
+public class FutureDemo {
     public static void main(String[] args) throws InterruptedException {
         long startTime = System.currentTimeMillis();
         //Test one by one
@@ -22,7 +19,7 @@ public class CompletableFutureDemo {
     }
 }
 
-class CompletableFutureDemo2 {
+final class FutureDemo2 {
     static ExecutorService service = Executors.newFixedThreadPool(3);
 
     public static void main(String[] args) {
@@ -63,8 +60,17 @@ class CompletableFutureDemo2 {
         System.out.println("----cost time: " + (endTime - startTime) + "ms");
     }
 }
-
-class CompletableFutureDemo3 {
+/**
+ * Once using the method {@link Future#get()}, it may be blocked.
+ * Because its calculating is not finished.
+ * If you use the method {@link Future#isDone()}, It may cause CPU wasting of resources
+ * <p>
+ * Conclusion: The result of getting is unfriendly in class {@link Future}.
+ * Only by blocking or polling can we get it.
+ * </p>
+ * @author Bloduc Spauter
+ */
+final class FutureDemo3 {
     public static void main(String[] args) throws ExecutionException, InterruptedException {
         FutureTask<String> futureTask = new FutureTask<>(() -> {
             System.out.println(Thread.currentThread().getName() + "------Come In");
@@ -75,12 +81,8 @@ class CompletableFutureDemo3 {
         Thread thread = new Thread(futureTask, "t1");
         thread.start();
         long startTime = System.currentTimeMillis();
-        /*
-            Once using the method "get", it may be blocked because it's calculate is not finished
-            If you use method "isDome", It may cause CPU idling
-            Uncomment this below code, The following loop statement needs to wait the thread done
-         */
-//        System.out.println(futureTask.get());
+        /*  Uncomment this below code, The following loop statement needs to wait the thread done*/
+        //System.out.println(futureTask.get());
         System.out.println(Thread.currentThread().getName() + "------Busy for other task");
         long endTime = System.currentTimeMillis();
         System.out.println("----cost time: " + (endTime - startTime) + "ms");
@@ -94,5 +96,63 @@ class CompletableFutureDemo3 {
                 System.out.println("The task t1 is running, don't urge!");
             }
         }
+    }
+}
+
+/**
+ * Using class {@link Future} is OK for simple business.
+ * It is a call-back notification that {@link Future} informs we when finished a task.
+ * <p>
+ * Using method {@link Future#isDone()} may waste CPU resources.
+ * <p>
+ * To create an asynchronous tasks, we need to combine {@link Future} with the thread poll.
+ * @author Bloduc Spauter
+ */
+final class FutureDemo4{
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        /*
+        If you don't have a specified Executor way,
+        you can use the default thread pool "ForkJoinPool.commonPool",
+        and as its thread pool to run this asynchronous code.
+         */
+        ExecutorService executorService=Executors.newFixedThreadPool(3);
+        //Running an asynchronous method, and no a return statement
+        CompletableFuture<Void> completableFuture=CompletableFuture.runAsync(()->{
+            //ForkJoinPool-worker-3
+            System.out.println(Thread.currentThread().getName());
+            try {
+                //sleep 1s
+                TimeUnit.MICROSECONDS.sleep(1000000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+                //The result is "pool-1-thread-1"
+                //Comment this below code;
+                // the first sentence printed will turn "ForkJoinPool-worker-1"
+                ,executorService
+        );
+        //null.
+        executorService.shutdown();
+        System.out.println(completableFuture.get());
+    }
+}
+
+final class FutureDemo5{
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        //It will return a String statement
+        CompletableFuture<String> completableFuture=CompletableFuture.supplyAsync(()->{
+            System.out.println(Thread.currentThread().getName());
+            try {
+                //sleep 1s
+                TimeUnit.MICROSECONDS.sleep(1000000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            return "Hello!This is  supplyAsync";
+        });
+        //Hello!This is supplyAsync
+        System.out.println(completableFuture.get());
+
     }
 }
